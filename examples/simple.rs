ff92dc6a46a8f12d4533c52f3627da993f6a103f
aumetra <aumetra@cryptolab.net>
2023-11-16 21:35:42 +0100

    Add simple example reading from stdin

---
+++ examples/simple.rs
@@ -0,0 +1,19 @@
+use bubble_bath::BubbleBath;
+use std::{
+    error::Error,
+    io::{self, Read},
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    let sanitizer = BubbleBath {
+        preserve_escaped: true,
+        ..BubbleBath::default()
+    };
+    let output = sanitizer.clean(&input).unwrap();
+    println!("{output}");
+
+    Ok(())
+}
